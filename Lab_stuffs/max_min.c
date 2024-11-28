94e9cfb99641959b9a63f818943207cac6bc8726
Vaishali Bhatt <vaishalibhatt46@gmail.com>
2024-11-28 17:28:51 +0530

    Create max_min.c

---
+++ Lab_stuffs/max_min.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+int main() { int a,b,c; printf("ENTER THE VALUE:"); scanf("%d%d%d",&a,&b,&c);
+
+if((a>b)&&(a>c)) { printf("MAXIMUM NUMBER:%d",a); } else if((b>a)&&(b>c)) { printf("MAXIMUM VALUE:%d",b); } else { printf("MAXIMUM NUMBER:%d",c); } return 0; }
